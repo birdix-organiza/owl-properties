be9071fb8f07763246bfb79b4b36a99625068907
ChaoqiYin <305111632@qq.com>
2025-07-21 14:24:00 +0800

    feat(renderer): 更新 Select 组件并增加动态选项支持
    - 增加 options 计算属性，支持动态生成选项
    - 修改模板，使用新的 options 属性
    - 更新包版本至 2.1.0

--- src/components/renderer/Select.ts
+++ src/components/renderer/Select.ts
@@ -11,13 +11,21 @@ export class Select extends BaseRenderer {
   t-on-change="onChangeSelect"
 >
   <option t-if="props.placeholder" value="" selected="!state.value">{{props.placeholder}}</option>
-  <t t-foreach="props.extra?.options || []" t-as="option" t-key="option.value">
+  <t t-foreach="options" t-as="option" t-key="option.value">
     <option t-att-value="option.value">
       <t t-esc="option.label" />
     </option>
   </t>
 </select>
   `;
+
+  get options() {
+    if (this.props.extra?.options) {
+      return typeof this.props.extra.options === 'function' ? this.props.extra.options() : this.props.extra.options;
+    }
+
+    return [];
+  }
 
   format(value: any) {
     return value ?? '';
